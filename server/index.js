57f3a14cb20698aea2cfd8f87ba50e9e954e7dde
aman8146 <amanupadhyay8146@gmail.com>
2025-05-22 00:43:19 +0530

    Add additional CORS origin for mobile access

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','http://172.16.122.96:5173'],
     credentials: true,  
     methods: 'GET,POST,PUT,DELETE,OPTIONS',  
   };
